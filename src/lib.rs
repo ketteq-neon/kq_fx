f45c1daa334e278218181950b26e1c696e4e9149
Giancarlo A. Chiappe <gchiappe@linkfast.io>
2024-06-27 02:26:50 -0500

    created new function, added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ use pgrx::prelude::*;
 use pgrx::shmem::*;
 use pgrx::spi::SpiResult;
 use pgrx::{debug1, error, pg_shmem_init, GucContext, GucFlags, GucRegistry, GucSetting};
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
 use std::ffi::CStr;
 
 // Max allocation params
@@ -303,23 +301,6 @@ fn validate_compatible_db() -> &'static str {
     }
 }
 
-// Utility
-
-#[derive(Debug, Serialize, Deserialize)]
-#[serde(untagged)]
-enum DependencyValue {
-    String(String),
-    Object {
-        version: String,
-        features: Vec<String>,
-    },
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct CargoToml {
-    dependencies: HashMap<String, DependencyValue>,
-}
-
 // Exported Functions
 
 #[pg_extern]
@@ -441,6 +422,34 @@ fn kq_fx_get_rate_xuid(
     kq_fx_get_rate(*from_id, *to_id, date)
 }
 
+
+#[pg_extern(parallel_safe)]
+fn kq_get_arr_value(dates: Vec<ExtDate>, values: Vec<f64>, date: ExtDate, default_value: Option<f64>) -> Option<f64> {
+    if dates.is_empty() || values.is_empty() {
+        return default_value;
+    }
+
+    if dates.len() != values.len() {
+        error!("dates and values arrays does not have the same quantity of elements")
+    }
+
+    let dates: Vec<i32> = dates.iter().map(|date| date.to_pg_epoch_days()).collect();
+    let date = date.to_pg_epoch_days();
+
+    let pos = match dates.binary_search(&date) {
+        Ok(idx) => idx, // exact match
+        Err(idx) => {
+            if idx == 0 { // date precedes first element
+                return default_value;
+            } else { // <= value
+                idx - 1
+            }
+        }
+    };
+
+    values.get(pos).copied().or(default_value)
+}
+
 #[cfg(any(test, feature = "pg_test"))]
 #[pg_schema]
 mod tests {
@@ -501,6 +510,58 @@ mod tests {
                 1,
                 pgrx::Date::new(2100, 1, 1).unwrap() // Max Date: 2024-03-01
             )
+        );
+    }
+
+    fn create_date(year: i32, month: u8, day: u8) -> Date {
+        Date::new(year, month, day).expect("Failed to create date")
+    }
+
+    #[pg_test]
+    fn test_kq_get_arr_value() {
+        let dates = vec![
+            create_date(2000, 1, 1),
+            create_date(2000, 1, 2),
+            create_date(2000, 1, 3),
+            create_date(2000, 1, 5),
+            create_date(2000, 1, 8),
+        ];
+        let values = vec![10.0, 20.0, 30.0, 50.0, 80.0];
+        let default_value = Some(0.0);
+        // intermediate date (within range but not an exact match)
+        assert_eq!(
+            crate::kq_get_arr_value(dates.clone(), values.clone(), create_date(2000, 1, 4), default_value),
+            Some(30.0)
+        );
+
+        // exact date match
+        assert_eq!(
+            crate::kq_get_arr_value(dates.clone(), values.clone(), create_date(2000, 1, 8), default_value),
+            Some(80.0)
+        );
+
+        // date before any value in dates
+        assert_eq!(
+            crate::kq_get_arr_value(dates.clone(), values.clone(), create_date(1999, 12, 31), default_value),
+            default_value
+        );
+
+        // date after the last value in dates
+        assert_eq!(
+            crate::kq_get_arr_value(dates.clone(), values.clone(), create_date(2000, 1, 9), default_value),
+            Some(80.0)
+        );
+
+        // dates and values empty
+        assert_eq!(
+            crate::kq_get_arr_value(vec![], vec![], create_date(2000, 1, 9), default_value),
+            default_value
+        );
+
+        // exact date match to the first value in dates
+        assert_eq!(
+            crate::kq_get_arr_value(dates.clone(), values.clone(), create_date(2000, 1, 1), default_value),
+            Some(10.0)
         );
     }
 }
